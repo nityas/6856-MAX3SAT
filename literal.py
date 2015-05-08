039fdabba4c9cd7dfa150d4aa1ed045785677b27
Nitya Subramanian <nityas@mit.edu>
2015-05-08 15:25:24 -0400

    adding more robust rounding

--- literal.py
+++ literal.py
@@ -1,14 +1,29 @@
 import random
 import pycosat
 
-#generate a random assignment
+#######
+#generate a random assignment with each var set to 
+#true with probability p, where p is either a list
+#of assignment probabilites or a single value
+#######
 def round(inp,p):
 	num_literals = num_literals(inp)
+	if type(p) is float:
+		p_is_list = False
+	elif (type(p) is list) and (len(p) == num_literals):
+		p_is_list = True
+	else:
+		return "invalid p"
+
 	assignment = [0 for i in range(num_literals)]
 	for literal in range(num_literals):
 		r = random.random()
+		if p_is_list:
+			p_x = p[literal-1]
+		else:
+			p_x = p
 		# set to false
-		if random > p:
+		if random > p_x:
 			assignment[literal-1]= -1*literal
 		# set to true with probability p
 		else:
