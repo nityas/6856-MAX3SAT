65f090cee6987549f65e49bb6472b7f60bd4f9a1
Chennah Heroor <chennah@Chennahs-MacBook-Pro-2.local>
2015-05-09 16:55:04 -0400

    updated name of GW method

--- combo.py
+++ combo.py
@@ -1,20 +1,12 @@
 from literal import *
 from GWlp import *
 
-def combo_max_sat(inp,p=0.5):
-	gw_sol = gw_max_sat(inp)
-	result_str = "\nGW Solution: "+ gw_sol
-
+def combo_soln(inp,p=0.5):
+	gw_sol = GW(inp)
 	naive_sol = naive_max_sat(inp,p)
-	result_str += "\nNaive Approx Solution: "+ naive_sol
-
-	dpll_sol = dpll(inp, literals(inp))[0]
-	result_str += "\nExact Solution: "+ naive_sol
-
 	if gw_sol > naive_sol:
-		result_str += "\nGW Better: "+ gw_sol
+		return ("gw",gw_sol,naive_sol)
 	elif gw_sol == naive_sol:
-		result_str += "\nEqual: "+ gw_sol
+		return ("same",gw_sol,naive_sol)
 	else:
-		result_str += "\nNaive Better: "+ naive_sol
-	return result_str
+		return ("naive",gw_sol,naive_sol)
