5090deb71f27358e05bd5539191ec32996b419ef
Nitya Subramanian <nityas@mit.edu>
2015-05-08 22:58:20 -0400

    added naive end-to-end and combo solver stub

---
+++ combo.py
@@ -0,0 +1,12 @@
+from literal import *
+from GWlp import *
+
+def combo_soln(inp,p=0.5):
+	gw_sol = gw_max_sat(inp)
+	naive_sol = naive_max_sat(inp,p)
+	if gw_sol > naive_sol:
+		return ("gw",gw_sol,naive_sol)
+	elif gw_sol == naive_sol:
+		return ("same",gw_sol,naive_sol)
+	else:
+		return ("naive",gw_sol,naive_sol)
