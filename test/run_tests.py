b1899c6b6dc0968cc2b31c8df3421eee6e854539
Nitya Subramanian <nityas@mit.edu>
2015-05-09 00:14:31 -0400

    added back run_tests

---
+++ test/run_tests.py
@@ -0,0 +1,35 @@
+import os
+
+def cnf_to_python(file):
+	f=open(file)
+	instance = []
+	for line in f.readlines():
+		line.replace('\t',' ')
+		line.strip('\n')
+		line_tokens = line.split(' ')
+		if 'c' in line_tokens[0] or 'p' in line_tokens[0]:
+			continue
+		else:
+			line_tokens = [int(i) for i in line_tokens[0:-1]]
+			if len(line_tokens) == 0:
+				continue
+			instance.append(line_tokens)
+	return instance
+
+#creates an array of testcases
+def load_testcases():
+	testcases = []
+	for filename in os.listdir('testcases'):
+		testcases.append(cnf_to_python('testcases/'+filename))
+	return testcases
+
+def run_testcases(testcases):
+	pass
+
+def main():
+	testcases = load_testcases()
+	run_testcases(testcases)
+
+if __name__ == '__main__':
+	main()
+
