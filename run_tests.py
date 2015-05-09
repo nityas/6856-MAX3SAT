a1319fc7da3c11920a884f3a04594e6deb360f6c
Chennah Heroor <chennah.heroor@gmail.com>
2015-05-09 19:47:55 -0400

    committing to pull

--- run_tests.py
+++ run_tests.py
@@ -6,12 +6,14 @@ def cnf_to_python(file):
         f=open(file)
         instance = []
         for line in f.readlines():
+                print line
                 line.replace('\t',' ')
                 line.strip('\n')
                 line_tokens = line.split(' ')
                 if 'c' in line_tokens[0] or 'p' in line_tokens[0]:
                         continue
                 else:
+                        print line_tokens
                         line_tokens = [int(i) for i in line_tokens[0:-1]]
                         if len(line_tokens) == 0:
                                 continue
@@ -26,19 +28,18 @@ def load_testcases():
         return testcases
 
 def run_testcases(testcases):
-	f = open('results_'+str(int(time.time()))+'.txt','w+')
-	for filename, instance in testcases:
-		f.write(filename+'\n')
-		f.flush()
-		var_prob = GW(instance)
-		for p in range(0,11,1):
-			rounded = rand_round(instance, var_prob)
-			sol = satisfied_clauses(instance, rounded)
-			result = combo_max_sat(instance, sol, float(p)/10)
-			output = str(result)
-			f.write(output) 
-			f.flush()
-	f.close()
+        f = open('results_'+str(int(time.time()))+'.txt','aw')
+        for filename, instance in testcases:
+                f.write(filename)
+                var_prob = GW(instance)
+                for p in range(0,11,1):
+                        rounded = rand_round(instance, var_prob)
+                        sol = satisfied_clauses(instance, rounded)
+                        result = combo_soln(instance, sol, float(p)/10)
+                        output = str(result)
+                        f.write(output) 
+        f.close()
+
 
 
 def main():
@@ -47,5 +48,4 @@ def main():
 
 if __name__ == '__main__':
         main()
-
 
